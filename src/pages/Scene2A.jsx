c897f5114710f02d7d63514d219e907d433d4e3f
edboyarina <boyarina123@gmail.com>
2025-06-25 12:54:13 +0300

    Выбор1

---
+++ src/pages/Scene2A.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SceneType1 from '../components/SceneType1';
+
+function Scene2A({ onNext }) {
+  return (
+    <SceneType1
+      background="/библиотека1.jpg"
+      characterName=""
+      text="Тебе стало скучно и ты вышла пораньше. Ты блуждаешь среди учебников, думая о том, что до экзамена и впраду осталось немного... "
+      onNext={onNext}
+    />
+  );
+}
+
+export default Scene2A;
