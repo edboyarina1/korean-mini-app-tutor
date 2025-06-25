044629d88b6494ac7a6f59a8b9562e15b13e3b3d
edboyarina <boyarina123@gmail.com>
2025-06-25 11:15:03 +0300

    Adding scenetype1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import WelcomeScene from './pages/WelcomeScene';
 import IntroScene from './pages/IntroScene';
+import Scene1 from './pages/Scene1';
+import Scene2 from './pages/Scene2';
 import './App.css';
 
 function App() {
@@ -12,10 +14,24 @@ function App() {
     setScene('intro');
   };
 
+  const handleIntroNext = () => {
+    setScene('scene1');
+  };
+
+  const handleScene1Next = () => {
+    setScene('scene2');
+  };
+
+  const handleScene2Next = () => {
+    alert("Конец демо!"); 
+  };
+
   return (
     <>
       {scene === 'welcome' && <WelcomeScene onStart={handleStart} />}
-      {scene === 'intro' && <IntroScene name={userName} />}
+      {scene === 'intro' && <IntroScene name={userName} onNext={handleIntroNext} />}
+      {scene === 'scene1' && <Scene1 onNext={handleScene1Next} />}
+      {scene === 'scene2' && <Scene2 onNext={handleScene2Next} />}
     </>
   );
 }
